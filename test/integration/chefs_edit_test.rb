60dfb08bc65932d47495bd2501da9f94243fe789
Michael Burton <gmichaelburton@gmail.com>
2017-12-22 19:54:12 +0000

    Complete Authentication System with admin and tests

--- test/integration/chefs_edit_test.rb
+++ test/integration/chefs_edit_test.rb
@@ -7,6 +7,10 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create!(chefname: "michael", email: "burtm@fordav.com",
                         password: "password", password_confirmation: "password")
+    @chef2 = Chef.create!(chefname: "mike", email: "mike@example.com",
+                            password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "mike2", email: "mike2@example.com",
+                            password: "password", password_confirmation: "password", admin: true)                    
   end
   
   test "reject an invalid edit" do
@@ -19,17 +23,41 @@ class ChefsEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.panel-body'
   end
   
-  test "accept valid signup" do
+  test "accept valid edit" do
     sign_in_as(@chef, "password")
     get edit_chef_path(@chef)
     assert_template 'chefs/edit'
-    patch chef_path(@chef), params: { chef: { chefname: "michael1 ", email: "burtm1@fordav.com" }}
+    patch chef_path(@chef), params: { chef: { chefname: "michael1", email: "burtm1@fordav.com" }}
     assert_redirected_to @chef
     assert_not flash.empty?
     @chef.reload
     assert_match "michael1", @chef.chefname
     assert_match "burtm1@fordav.com", @chef.email
     
+  end
+  
+  test "accept edit attempt by admin user" do
+    sign_in_as(@admin_user, "password")
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: { chef: { chefname: "michael3", email: "burtm3@fordav.com" }}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "michael3", @chef.chefname
+    assert_match "burtm3@fordav.com", @chef.email
+  end
+  
+  test "redirect edit attempt by another non-admin user" do
+    sign_in_as(@chef2, "password")
+    updated_name = "joe"
+    updated_email = "joe@example.com"
+    patch chef_path(@chef), params: { chef: { chefname: updated_name, email: updated_email }}
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "michael", @chef.chefname
+    assert_match "burtm@fordav.com", @chef.email
   end
   
 end
--- test/integration/chefs_listing_test.rb
+++ test/integration/chefs_listing_test.rb
@@ -9,6 +9,9 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
                             password: "password", password_confirmation: "password")
     @chef2 = Chef.create!(chefname: "mike", email: "mike@example.com",
                             password: "password", password_confirmation: "password")
+    @admin_user = Chef.create!(chefname: "mike2", email: "mike2@example.com",
+                            password: "password", password_confirmation: "password", admin: true)
+                            
   end
   
   test "should get chefs index" do
@@ -24,11 +27,11 @@ class ChefsListingTest < ActionDispatch::IntegrationTest
   end
   
   test "should delete chef" do
-    sign_in_as(@chef, "password")
+    sign_in_as(@admin_user, "password")
     get chefs_path
     assert_template 'chefs/index'
     assert_difference 'Chef.count', -1 do
-      delete chef_path(@chef)
+      delete chef_path(@chef2)
     end
     assert_redirected_to chefs_path
     assert_not flash.empty?
