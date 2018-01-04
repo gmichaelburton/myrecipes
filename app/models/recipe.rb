14fae3f1d5bfad7a6ed0ebbd8847a0915f8be0ab
Michael Burton <gmichaelburton@gmail.com>
2018-01-04 22:42:00 +0000

    Add comment model and associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,9 +5,10 @@ class Recipe < ApplicationRecord
   belongs_to :chef
   
   validates :chef_id, presence: true
-  default_scope -> { order(updated_at: :desc)}
+  default_scope -> { order(updated_at: :desc)} #shows newest instance at the top
   
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+  has_many :comments, dependent: :destroy
   
 end
